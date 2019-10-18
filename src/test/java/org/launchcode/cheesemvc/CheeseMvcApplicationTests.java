dea0658efa2a56c6312e92e0ef1c243b83823216
Wesley Park <wespark25@gmail.com>
2019-10-18 13:04:03 -0500

    Initial commit

---
+++ src/test/java/org/launchcode/cheesemvc/CheeseMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.cheesemvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheeseMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
